4d0b28870cb66d3e8b3c40c0f745b31b967d2dd5
Lucas Campelo <dlucascampelo@gmail.com>
2021-12-26 18:33:42 -0300

    Update: creating Gofinances and set global theme

---
+++ GoFinances/src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: "#5636D3",
+
+    secondary: "#FF872C",
+    secondary_light: "rgba(255,145,44,0.3)",
+
+    success: "#12A454",
+    success_light: "rgba(18,164,84,0.5)",
+
+    attention: "#E83F5B",
+    attention_light: "rgba(232,63,91,0.5)",
+
+    title: "#363F5F",
+    text: "#969CB2",
+    shape: "#FFFFFF",
+    background: "#F0F2F5",
+  }
+}
