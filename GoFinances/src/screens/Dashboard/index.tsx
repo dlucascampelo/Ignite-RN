38fe46086a951e55938edab017266131b9cb4752
Lucas Campelo <dlucascampelo@gmail.com>
2021-12-27 22:17:06 -0300

    Update:finishing header and card component

--- GoFinances/src/screens/Dashboard/index.tsx
+++ GoFinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
+import {
+  Container,
+  Header,
+  UserInfo,
+  User,
+  UserWrapper,
+  Photo,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards
+} from './styles'
 
-import { Container, Title } from './styles'
 export function Dashboard() {
 
   return (
     <Container>
-      <Title>Dashboard</Title>
-    </Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/49032660?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lucas</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+
+      </HighlightCards>
+    </Container >
   )
 }
