0f074d79e01ef68540d4d0ded3ec35df7852d545
Lucas Campelo <dlucascampelo@gmail.com>
2021-12-29 20:19:06 -0300

    Update: creating transaction cards and the types

--- GoFinances/src/screens/Dashboard/styles.ts
+++ GoFinances/src/screens/Dashboard/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native'
 import { Feather } from '@expo/vector-icons'
+import { FlatList, FlatListProps } from 'react-native'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
-import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { DataListProps } from '.'
 
 export const Container = styled.View`
 flex: 1;
@@ -70,4 +72,27 @@ width:100%;
 
 position:absolute;
 margin-top:${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+flex:1%;
+padding: 0 24px;
+margin-top:${RFPercentage(15)}px;
+`;
+
+export const Title = styled.Text`
+font-family:${({ theme }) => theme.fonts.regular};
+font-size:${RFValue(18)}px;
+color:${({ theme }) => theme.colors.title};
+
+margin-bottom:16px;
+`;
+
+export const TransactionsList = styled(FlatList as new (props: FlatListProps<DataListProps>) => FlatList<DataListProps>).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace()
+  }
+})`
+
 `;
