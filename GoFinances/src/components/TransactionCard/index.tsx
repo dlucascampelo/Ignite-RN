0b3d5dbaaace05736420ff49cba95cb0377bc904
Lucas Campelo <dlucascampelo@gmail.com>
2022-01-12 20:25:45 -0300

    feat: list transactions

--- GoFinances/src/components/TransactionCard/index.tsx
+++ GoFinances/src/components/TransactionCard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { categories } from '../../utils/categories';
 import {
   Container,
   Title,
@@ -11,25 +11,23 @@ import {
   Date
 } from './styles'
 
-interface CategoryProps {
-  name: string;
-  icon: string;
-}
 export interface TransactionCardProps {
   type: 'positive' | 'negative',
-  title: string;
+  name: string;
   amount: string;
-  category: CategoryProps;
+  category: string;
   date: string;
 }
 interface Props {
   data: TransactionCardProps
 }
 export function TransactionCard({ data }: Props) {
+  const [category] = categories.filter(item => item.key === data.category
+  );
 
   return (
     <Container>
-      <Title>{data.title}</Title>
+      <Title>{data.name}</Title>
       <Amount type={data.type}>
         {data.type === 'negative' && '- '}
         {data.amount}
@@ -37,8 +35,8 @@ export function TransactionCard({ data }: Props) {
 
       <Footer>
         <Category>
-          <Icon name={data.category.icon} />
-          <CategoryName>{data.category.name}</CategoryName>
+          <Icon name={category.icon} />
+          <CategoryName>{category.name}</CategoryName>
         </Category>
         <Date>{data.date}</Date>
       </Footer>
