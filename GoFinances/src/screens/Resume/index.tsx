aa9260f19200e264df85299655b3b44a5e1cf9b3
Lucas Campelo <dlucascampelo@gmail.com>
2022-01-14 23:41:25 -0300

    chore:minors update

---
+++ GoFinances/src/screens/Resume/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { HistoryCard } from '../../components/HistoryCard'
+import {
+  Container,
+  Header,
+  Title
+} from './styles'
+
+export function Resume() {
+  return (
+
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+      <HistoryCard />
+
+
+
+
+
+
+
+
+    </Container>
+  )
+}
--- GoFinances/src/screens/Resume/styles.ts
+++ GoFinances/src/screens/Resume/styles.ts
@@ -19,20 +19,3 @@ color:${({ theme }) => theme.colors.shape};
 font-family:${({ theme }) => theme.fonts.regular};
 font-size:${RFValue(18)}px;
 `;
-
-export const Form = styled.View`
-flex:1;
-justify-content: space-between;
-width:100%;
-padding:24px;
-`;
-
-export const Fields = styled.View``;
-
-export const TransactionsTypeBox = styled.View`
-flex-direction:row;
-justify-content:space-between;
-margin-top: 8px;
-margin-bottom:16px;
-`;
-
