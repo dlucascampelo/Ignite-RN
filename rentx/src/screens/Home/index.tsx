a21c3a29f982157116c6d73ebac4e38ceb63e789
Lucas Campelo <dlucascampelo@gmail.com>
2022-02-01 17:37:52 -0300

    feat: set initial project configs

---
+++ rentx/src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Container,
+  Title,
+} from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
